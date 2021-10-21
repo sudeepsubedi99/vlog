d947e0120323287bc5d377cec264651dc2ef6c15
sudeepsubedi99 <50973728+sudeepsubedi99@users.noreply.github.com>
2021-10-21 13:03:44 +0545

    Models and MIgration , seeder

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +20,9 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
