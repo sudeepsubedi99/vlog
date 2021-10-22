8c9a2a757cf6f2ca6afce6dfdf280fd5e3b7d814
sudeepsubedi99 <50973728+sudeepsubedi99@users.noreply.github.com>
2021-10-22 12:16:32 +0545

    relationship in models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use phpDocumentor\Reflection\Types\This;
 
 class Post extends Model
 {
     use HasFactory;
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
