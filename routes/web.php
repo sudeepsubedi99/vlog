acca6e9613a2dfce2abeb2ad781387a78c5cd66c
sudeepsubedi99 <50973728+sudeepsubedi99@users.noreply.github.com>
2021-10-25 22:10:40 +0545

    Adminlte install

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,9 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', function() {
+    return view('home');
+})->name('home')->middleware('auth');
